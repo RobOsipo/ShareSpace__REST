cb24b96bb2f801af7d0e1f4389524f0d8f15414a
Robert <osipovitchr@gmail.com>
2022-05-23 12:35:08 -0400

    moved my current routes logic to seperate controllers

--- routes/places.js
+++ routes/places.js
@@ -1,50 +1,14 @@
 const express = require("express");
 
 const HttpError = require("../models/errors");
+const placesController = require('../controllers/places')
 const router = express.Router();
 
-const DUMMY = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "Famous Sky Scraper",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
 
 // Find place by id
-router.get("/:id", (req, res, next) => {
-  const { id } = req.params;
-  const foundPlace = DUMMY.find((place) => {
-    return place.id === id;
-  });
+router.get("/:id", placesController.getPlaceById);
 
-  if (!foundPlace) {
-    throw new HttpError("Could Not Find A Place With That ID", 404);
-  }
-
-  res.json({ message: "Found Place by ID Successful", place: foundPlace });
-});
-
-// Find place by creator ID
-router.get("/user/:id", (req, res, next) => {
-  const { id } = req.params;
-  const foundPlace = DUMMY.find((place) => {
-    return place.creator === id;
-  });
-
-  if (!foundPlace) {
-    return next(
-      new HttpError("Could Not Find A Place With That Creator ID", 404)
-    );
-  }
-
-  res.json({ message: "Found User by ID Successful", place: foundUser });
-});
+// Find place by Creator ID
+router.get('/users/:id', placesController.getPlaceByCreatorId)
 
 module.exports = router;
