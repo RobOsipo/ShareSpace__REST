3700783d694cc610e1126488335021e7ce273c49
Robert <osipovitchr@gmail.com>
2022-05-26 15:36:53 -0400

    all places controllers refactored from using dummy code to MongoDB

--- controllers/places.js
+++ controllers/places.js
@@ -5,19 +5,6 @@ const getCoordsForAddress = require("../util/location");
 
 const PlaceModel = require("../models/place");
 
-let DUMMY = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "Famous Sky Scraper",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
 
 const getPlaceById = async (req, res, next) => {
   const { id } = req.params;
@@ -37,7 +24,7 @@ const getPlaceById = async (req, res, next) => {
 
   res.json({
     message: "Found Place by ID Successful",
-    place: foundPlace.toObject({ getters: true })
+    place: foundPlace.toObject({ getters: true }),
   });
 };
 
@@ -58,9 +45,10 @@ const getPlacesByCreatorId = async (req, res, next) => {
     );
   }
 
-  res.json({                    // ! when using toObject({ getters: true}) it takes the _ away from _id
+  res.json({
+    // ! when using toObject({ getters: true}) it takes the _ away from _id
     message: "Found Places by Creator ID Successful",
-    places: foundPlaces.map(place => place.toObject({ getters: true }))
+    places: foundPlaces.map((place) => place.toObject({ getters: true })),
   });
 };
 
@@ -104,7 +92,7 @@ const createPlace = async (req, res, next) => {
   });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -114,25 +102,36 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const { id } = req.params;
 
-  const updatedPlace = DUMMY.find((place) => place.id === id);
-  const copy = { ...updatedPlace };
-  const placeIndex = DUMMY.findIndex((place) => place.id === id);
-  copy.title = title;
-  copy.description = description;
-
-  DUMMY[placeIndex] = copy;
-
-  res.status(200).json({ message: "Update Place Successful", place: copy });
-};
-
-const deletePlace = (req, res, next) => {
-  const { id } = req.params;
-
-  if (!DUMMY.find((place) => place.id === id)) {
-    throw new HttpError("Could not Find A Place With That ID", 404);
+  let foundPlacetoUpdate;
+  try {
+    foundPlacetoUpdate = await PlaceModel.findByIdAndUpdate(id, {
+      title: title,
+      description: description,
+    });
+  } catch (err) {
+    const error = new HttpError("Could Not Update Place", 500);
+    return next(error);
   }
 
-  DUMMY = DUMMY.filter((place) => place.id !== id);
+  res
+    .status(200)
+    .json({
+      message: "Update Place Successful",
+      place: foundPlacetoUpdate.toObject({ getters: true }),
+    });
+};
+
+const deletePlace = async (req, res, next) => {
+  const { id } = req.params;
+
+  let foundPlaceToDelete;
+  try {
+    foundPlaceToDelete = await PlaceModel.findByIdAndRemove(id);
+  } catch (err) {
+    const error = new HttpError("Could Not Find The Place To Delete", 500);
+    return next(error);
+  }
+
   res.status(200).json({ message: "Place Deleted Successfully" });
 };
 
